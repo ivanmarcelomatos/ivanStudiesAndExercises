8edb106c7cf3fa2e1e313db240f31c48f34c049f
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2024-05-10 14:03:44 -0300

    Attaching listeners to groups of nodes - exemple 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events
-
+<https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events#attaching-listeners-to-groups-of-nodes>
 
 Events
-Method 3 - with named function and event listener and parameter example - The "e" parameter: e.target.style.background = "blue";
+Attaching listeners to groups of nodes;
--- js-file.js
+++ js-file.js
@@ -1,6 +1,11 @@
-const btn = document.querySelector("#btn");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-btn.addEventListener("click", function(e) {
-    console.log(e.target.style.background = 'blue');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
 });
   
