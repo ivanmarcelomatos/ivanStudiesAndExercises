564cc8374dc0b530272f83c5fd4bb4c856e7e33d
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-05-10 10:58:24 -0300

    Event listener with parameter example 1

--- js-file.js
+++ js-file.js
@@ -1,8 +1,6 @@
-function alertFunction() {
-    alert("YAY! YOU DID IT!");
-}
-
 const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", alertFunction);
+btn.addEventListener("click", function(e) {
+    console.log(e);
+});
   
