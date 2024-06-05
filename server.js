26177eb148fb96bd2ea0f6f23021383f18db0b0a
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-06-05 14:05:09 -0300

    solved

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write(req.url);
   res.end();
-}).listen(8080); 
+}).listen(8080);
 
 console.log('Server running at http://localhost:8080/');
