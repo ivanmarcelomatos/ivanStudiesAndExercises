110cbfedc0666ac37b48f34499da59fee4a4d517
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-06-07 14:31:35 -0300

    upload a file with formidable module

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-//Create an event handler:
-var myEventHandler = function () {
-  console.log('I hear a scream!');
-}
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
 
-//Assign the eventhandler to an event:
-eventEmitter.on('scream', myEventHandler);
 
-//Fire the 'scream' event:
-eventEmitter.emit('scream');
 
-              
-
+console.log('Server running at http://localhost:8080/');
