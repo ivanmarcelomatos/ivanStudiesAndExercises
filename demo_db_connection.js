742ba40636ad57ddc4e9c2dd018a2cadc36789c2
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-07-02 09:59:51 -0300

    Select only 'name' and 'address' columns from the 'customers' table, and display the return object

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -2,14 +2,14 @@ var mysql = require('mysql2');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "",
   password: "",
   database: "mydb"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM customers", function (err, result, fields) {
+  con.query("SELECT name, address FROM customers", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
