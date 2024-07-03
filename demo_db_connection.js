3764fc1a57c0213569acc41720f038866dcf62a7
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-07-03 08:08:17 -0300

    Escape query values by using the placeholder '?' method

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,43 +1,20 @@
 var mysql = require('mysql2');
 
-const con = mysql.createConnection({
-  host: 'localhost', 
-  user: '', 
-  password: '', 
-  database: 'mydb'
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "",
+  password: "",
+  database: "mydb"
 });
 
-con.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database as id', con.threadId);
-
-  // Example user input
-  const userInput = "Susan";
-  
-  // Use mysql.escape() to escape the user input
-  const escapedInput = mysql.escape(userInput);
-
-  // Use the escaped input in an SQL query
-  const sql = `SELECT * FROM customers WHERE name = ${escapedInput}`;
-
-  // Perform the query
-  con.query(sql, (err, results, fields) => {
-    if (err) {
-      console.error('Error executing query:', err.stack);
-      return;
-    }
-    console.log('Query results:', results);
-
-    // Close the connection
-    con.end((err) => {
-      if (err) {
-        console.error('Error closing the connection:', err.stack);
-        return;
-      }
-      console.log('Connection closed');
-    });
+con.connect(function(err) {
+  if (err) throw err;
+  var adr = 'Mountain 21';
+  //Escape the address value:
+  var sql = 'SELECT * FROM customers WHERE address = ?';
+  //Send an array with value(s) to replace the escaped values:
+  con.query(sql, [adr], function (err, result) {
+    if (err) throw err;
+    console.log(result);
   });
 });
