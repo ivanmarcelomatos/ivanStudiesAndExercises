e3c26c6f31c30dae0d3c6d974334dcb9f4ab9166
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-08-15 10:06:50 -0300

    Select all documents from a collection in mongoDB

--- demo_mongodb.js
+++ demo_mongodb.js
@@ -13,40 +13,10 @@ async function run() {
         const dbo = client.db('ivanBancoTeste');
 
         
-        const resultado = await dbo.collection('inventory').insertMany([
-            {
-              item: 'journal',
-              qty: 25,
-              size: { h: 14, w: 21, uom: 'cm' },
-              status: 'A'
-            },
-            {
-              item: 'notebook',
-              qty: 50,
-              size: { h: 8.5, w: 11, uom: 'in' },
-              status: 'A'
-            },
-            {
-              item: 'paper',
-              qty: 100,
-              size: { h: 8.5, w: 11, uom: 'in' },
-              status: 'D'
-            },
-            {
-              item: 'planner',
-              qty: 75,
-              size: { h: 22.85, w: 30, uom: 'cm' },
-              status: 'D'
-            },
-            {
-              item: 'postcard',
-              qty: 45,
-              size: { h: 10, w: 15.25, uom: 'cm' },
-              status: 'A'
-            }
-          ]);  
+        //const resultado = await dbo.collection('inventory').find({});
+        const resultado = await dbo.collection('inventory').find({}).toArray();
 
-        console.log("Documents added: ", resultado.insertedIds);
+        console.log("Result: ", resultado);
         
         await client.close();  
     } catch (error) {
