e81598ce885e452d380e0eff2107c3039b55b151
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-08-16 20:37:45 -0300

    Corrected example

--- demo_mongodb.js
+++ demo_mongodb.js
@@ -1,4 +1,4 @@
-//const { MongoClient } = require('mongodb'); 
+//const { MongoClient } = require('mongodb');
 const mongodb = require('mongodb').MongoClient;
 
 require('dotenv').config();
@@ -9,22 +9,22 @@ async function run() {
         const client = await mongodb.connect(process.env.DB_CONNECTION_STRING_URL);
 
         console.log('Connected to database');
-        
+       
         const dbo = client.db('ivanBancoTeste');
 
-        
-    
+       
+   
         const cursor = dbo.collection('inventory').find({
-            size: { w: 21, h: 14, uom: 'cm' }
+            size: { h: 14, w: 21, uom: 'cm' }
           });
         const resultado = await cursor.toArray();
 
         console.log("Result: ", resultado);
-        
-        await client.close();  
+       
+        await client.close(); 
     } catch (error) {
         console.error('Erro durante a operação:', error);
     }
 }
 
-run();  
+run(); 
