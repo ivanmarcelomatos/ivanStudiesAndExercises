77b8e4045162d2fbd0e5d007bb4769b38444c973
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-09-17 17:09:45 -0300

    Connect to a test database in your MongoDB instance then create the inventory collection

--- demo_mongodb.js
+++ demo_mongodb.js
@@ -14,22 +14,41 @@ async function run() {
 
 
 
-        await dbo.collection('cakeFlavors').updateOne(
+        await dbo.collection('inventory7').insertMany([
             {
-               $expr: { $eq: [ "$flavor", "$$targetFlavor" ] }
+              item: 'journal',
+              qty: 25,
+              size: { h: 14, w: 21, uom: 'cm' },
+              status: 'A'
             },
-            [
-               {
-                  $set: { flavor: "$$newFlavor" }
-               }
-            ],
             {
-               let: { targetFlavor: "cherry", newFlavor: "orange" }
+              item: 'notebook',
+              qty: 50,
+              size: { h: 8.5, w: 11, uom: 'in' },
+              status: 'P'
+            },
+            {
+              item: 'paper',
+              qty: 100,
+              size: { h: 8.5, w: 11, uom: 'in' },
+              status: 'D'
+            },
+            {
+              item: 'planner',
+              qty: 75,
+              size: { h: 22.85, w: 30, uom: 'cm' },
+              status: 'D'
+            },
+            {
+              item: 'postcard',
+              qty: 45,
+              size: { h: 10, w: 15.25, uom: 'cm' },
+              status: 'A'
             }
-         );
+          ]);
 
 
-        const cursor = dbo.collection('cakeFlavors').find();
+        const cursor = dbo.collection('inventory7').find();
 
         const resultado = await cursor.toArray();
 
