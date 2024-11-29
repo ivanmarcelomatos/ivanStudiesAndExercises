997da0bec5f0f260e6e7e83e3f8fd375986ed6d1
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-29 13:08:00 -0300

    test('there is no I in team'

--- filename.test.js
+++ filename.test.js
@@ -1,5 +1,3 @@
-test('adding floating point numbers', () => {
-  const value = 0.1 + 0.2;
-  //expect(value).toBe(0.3);           This won't work because of rounding error
-  expect(value).toBeCloseTo(0.3); // This works.
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
 });
