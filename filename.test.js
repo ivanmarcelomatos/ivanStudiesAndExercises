3a0a97197609cf6e433533fad78532af37ded741
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-29 16:23:42 -0300

    test('the shopping list has milk on it'

--- filename.test.js
+++ filename.test.js
@@ -1,3 +1,12 @@
-test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk',
+];
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
 });
