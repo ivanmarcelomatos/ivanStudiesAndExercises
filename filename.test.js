2a29b60069ee09ff748ef152f6bd021e0b6bbb69
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-29 07:57:28 -0300

    test('two plus two'

--- filename.test.js
+++ filename.test.js
@@ -1,8 +1,11 @@
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
 });
