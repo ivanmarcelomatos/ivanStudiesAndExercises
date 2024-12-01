0df21462d762dbfc4735e7faef0d6e51d00f12c0
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-01 19:43:27 -0300

    return all the posts that belong to the first user

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1', {
-    method: 'DELETE'
-})
+// This will return all the posts that belong to the first user
+fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
