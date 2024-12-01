af0e08ac90f566b19c85e994d69dad096d1a873e
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-01 17:20:36 -0300

    PATCH using fetch API

--- script.js
+++ script.js
@@ -1,56 +1,12 @@
-// GET com Fetch
-fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then((json) => {
-        console.log(json);
-    })
-      .catch((error) => {
-        console.log(error);
-      })
-
-//GET com Axios
-axios.get('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => {
-        console.log(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-
-
-
-// POST Data
-const data = {
-    title: "Some Title",
-    content: "Some Content...",
-    userId: 1
-}
-
-// POST com Fetch
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
     headers: {
-        "content-type": "application/json"
+        'Content-type': 'application/json; charset=UTF-8'
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+        title: 'PATCH using fetch API'
+    }),
 })
     .then(response => response.json())
-    .then((json) => {
-    console.log(json);
-    })
-    .catch((error) => {
-    console.log(error);
-    })
-
-
-
-
-
-    //POST com Axios
-    axios.post('https://jsonplaceholder.typicode.com/posts', data)
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
