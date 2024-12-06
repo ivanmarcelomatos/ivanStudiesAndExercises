9c1bc9435da93ae9521d372fd41399ed5183a182
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-06 15:10:15 -0300

    resolve

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// This is equivalent to /comments?postId=1
-fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// promise creation
+const myPromise = new Promise((resolve, reject) => {
+    const name = 'Matheus';
+
+    if(name === 'Matheus') {
+        resolve('user Matheus found');
+    } else {
+        reject('User Matheus was not found');
+    }
+});
+
+myPromise.then(data => {
+    console.log(data);
+});
