409e0b4d9672bb69821db58afdce0b4577bd1287
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-06 17:22:55 -0300

    Resolving multiple promises with the .all() method, one of which is slower

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // resolve multiple promises
 const p1 = new Promise((resolve, reject) => {
-    resolve('p1 ok!');
+    setTimeout(() => {
+        resolve('p1 ok! Timeout!');
+    }, 2000);
 });
 
 const p2 = new Promise((resolve, reject) => {
@@ -17,3 +19,5 @@ const p3 = new Promise((resolve, reject) => {
 const resolveAll = Promise.all([p1, p2, p3]).then((data) => {
     console.log(data);
 });
+
+console.log('after .all()');
