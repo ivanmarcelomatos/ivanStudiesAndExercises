b32ac4532dd9e45f8c53c7ea75a8aec626c8bab9
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-09 14:04:05 -0300

    test('the getGithubUserData() fails with an error', async () => {

--- filename.test.js
+++ filename.test.js
@@ -1,18 +1,22 @@
 const axios = require('axios');
 
-
 function getGithubUserData() {
   const username = 'ivanmarcelomatos';
 
-  return axios.get(`https://api.github.com/users/${username}`)
-      .then((response) => {
-          return response.data;
-      })
-};
+  return axios.get(`https://api.ERROR.github.com/users/${username}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Network error occurred');
+    });
+}
 
-
-
-test('the data.name is Ivan', async () => {
-  const data = await getGithubUserData();
-  expect(data.name).toBe('Ivan');
+test('the getGithubUserData() fails with an error', async () => {
+  expect.assertions(1);
+  try {
+    await getGithubUserData();
+  } catch (error) {
+    expect(error.message).toMatch('Network error occurred');
+  }
 });
