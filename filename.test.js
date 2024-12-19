e988ea9da98c8b8f6bd5012937ab60207aacc461
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-12-19 20:18:54 -0300

    test('the data.name is Ivan'

--- filename.test.js
+++ filename.test.js
@@ -1,17 +1,14 @@
 const axios = require('axios');
 
-
 function getGithubUserData() {
   const username = 'ivanmarcelomatos';
 
   return axios.get(`https://api.github.com/users/${username}`)
       .then((response) => {
           return response.data;
-      })
-};
+      });
+}
 
-
-
-test('the data.name is Ivan', async () => {
-  await expect(getGithubUserData()).resolves.toHaveProperty('name', 'Ivan');
+test('the data.name is Ivan', () => {
+  return expect(getGithubUserData()).resolves.toHaveProperty('name', 'Ivan');
 });
